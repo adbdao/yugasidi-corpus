4658d320af460c37d51ee35d17b358d3907ea706
magga <adb.dao@gmail.com>
2022-01-24 15:20:42 +0800

    to COR OK

---
+++ README.md
@@ -0,0 +1,8 @@
+# 《瑜伽師地論(標以韓清淨科文)》的原始碼 yugasidi for Accelon2017  
+  
+>> ## 台中南普陀佛學院教材講義：《瑜伽師地論》，含韓清淨科判
+---  
+> 展示網址：智邦主機 http://adbdao.url.tw/#c=yugasidi  
+  
+> 最新Accelon下載資訊 http://adbdao.blogspot.com/2018/07/blog-post.html  
+---  
---
+++ gen.js
@@ -0,0 +1,11 @@
+var createCorpusFromJSON=null;
+try{
+	createCorpusFromJSON=require("ksana-corpus-builder").createCorpusFromJSON;
+} catch(e){
+	createCorpusFromJSON=require("ksana-corpus-lib").createCorpusFromJSON;
+}
+
+createCorpusFromJSON("yugasidi-corpus.json",function(err,res){
+	console.log(res)
+	if (err)console.log(err);
+});
